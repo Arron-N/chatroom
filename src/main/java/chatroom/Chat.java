6313e88811d2da5040bc2cacf73ca0d7e04cbeda
Arron Nguyen <arron.nguyen@mrjaffesclass.com>
2023-05-31 14:21:00 -0700

    hi

---
+++ src/main/java/chatroom/Chat.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chatroom;
+
+/**
+ *
+ * @author student
+ */
+public class Chat {
+    
+    String username;
+    String message;
+    
+    public Chat(String username, String message) {
+        this.username = username;
+        this.message = message;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    
+}
